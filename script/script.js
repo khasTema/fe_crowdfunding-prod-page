e0a6f7a795273b3a4c7c279b511fbb5c2e4413d1
khasTema <akhasin.dev@gmail.com>
2021-11-16 19:12:58 +0200

    almoust done
    check JS for better logic in calculation

--- script/script.js
+++ script/script.js
@@ -1,15 +1,26 @@
 const menuShowBtn = document.getElementById('nav-show')
 const nav = document.getElementById('nav')
 const menuHideBtn = document.getElementById('nav-hide')
-
 const backProjectBtn = document.getElementById('back-project')
-
 const selectModal = document.getElementById('selction-modal')
 const successModal = document.getElementById('success-modal')
 const gotItBtn = document.getElementById('got-it')
-
+const selectRewardBtnArr = document.querySelectorAll('.option-card_btn')
 const continueBtnArr = document.querySelectorAll('.donation_continue-btn')
-// console.log(continueBtn)
+
+const moneyTotalDisplay = document.getElementById('money-total')
+const usersTotalDisplay = document.getElementById('users-total')
+const progressBar = document.getElementById('progress-fill')
+
+
+
+
+
+
+let totalFundedSum = parseInt(moneyTotalDisplay.innerText)
+// console.log(totalFundedSum +14)
+
+let totalUsers = parseInt(usersTotalDisplay.innerHTML)
 
 
 menuShowBtn.addEventListener('click', ()=>{
@@ -25,19 +36,30 @@ menuHideBtn.addEventListener('click', ()=>{
     menuShowBtn.style.display = 'block'
 })
 
+backProjectBtn.addEventListener('click', showSelectOptions)
 
-backProjectBtn.addEventListener('click', ()=>{
-    selectModal.style.display = 'block'
+selectRewardBtnArr.forEach((element) =>{
+    element.addEventListener('click', showSelectOptions)
 })
+
+function showSelectOptions(){
+    selectModal.style.display = 'block'
+}
 
 continueBtnArr.forEach((element) => {
     element.addEventListener('click', ()=>{
         selectModal.style.display = 'none'
         successModal.style.display = 'block'
+        totalUsers += 1
+        usersTotalDisplay.textContent = totalUsers
+        let donationSum = parseInt(element.previousElementSibling.children[1].value)
+        totalFundedSum += donationSum
+        moneyTotalDisplay.textContent = totalFundedSum
+        progressBar.style.width = '91%'
+  
     })
 })
 
 gotItBtn.addEventListener('click', ()=>{
     successModal.style.display = 'none'
 })
-
