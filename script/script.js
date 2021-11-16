59b1e3869f0efd1a9f737d9df1117f6200dbe7a7
khasTema <akhasin.dev@gmail.com>
2021-11-16 17:10:03 +0200

    basic js done
    now need to get calculations to work

--- script/script.js
+++ script/script.js
@@ -2,6 +2,15 @@ const menuShowBtn = document.getElementById('nav-show')
 const nav = document.getElementById('nav')
 const menuHideBtn = document.getElementById('nav-hide')
 
+const backProjectBtn = document.getElementById('back-project')
+
+const selectModal = document.getElementById('selction-modal')
+const successModal = document.getElementById('success-modal')
+const gotItBtn = document.getElementById('got-it')
+
+const continueBtnArr = document.querySelectorAll('.donation_continue-btn')
+// console.log(continueBtn)
+
 
 menuShowBtn.addEventListener('click', ()=>{
     console.log('you you')
@@ -15,3 +24,20 @@ menuHideBtn.addEventListener('click', ()=>{
     menuHideBtn.style.display = 'none'
     menuShowBtn.style.display = 'block'
 })
+
+
+backProjectBtn.addEventListener('click', ()=>{
+    selectModal.style.display = 'block'
+})
+
+continueBtnArr.forEach((element) => {
+    element.addEventListener('click', ()=>{
+        selectModal.style.display = 'none'
+        successModal.style.display = 'block'
+    })
+})
+
+gotItBtn.addEventListener('click', ()=>{
+    successModal.style.display = 'none'
+})
+
